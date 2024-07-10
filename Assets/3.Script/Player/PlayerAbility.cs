b00a7fb5b4e66c140054f0d0c40e7afd31b455a0
HongHyeon <toinbee3@naver.com>
2024-07-10 19:31:22 +0900

    Update PlayerAbility.cs

--- Assets/3.Script/Player/PlayerAbility.cs
+++ Assets/3.Script/Player/PlayerAbility.cs
@@ -1,13 +1,42 @@
 using UnityEngine;
 
 public class PlayerAbility : MonoBehavior {
+    public static PlayerAbility Instance = null;
+
+    private void Awake() {
+        if(Instance == null) { 
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+    //TODO: 굳이 싱글톤을 써야할 지 고민해보기
+    
     private float playerAttack;
     private float playerDefense;
     private float playerGather;
     private float playerSpeed;
-    private float playerAddInven;
-  
-      
+    private float playerInvenCount;
+    private float playerAddInvenCount;
+
+    public float GetPlayerAttack() {
+        return playerAttack;
+    }
+    public float GetPlayerDefense() {
+        return playerDefense;
+    }
+    public float GetPlayerGather() {
+        return playerGather;
+    }
+    public float GetPlayerSpeed() { 
+        return playerSpeed;
+    }
+    public float GetPlayerInvenCount() {
+        return playerInvenCount;
+    }
+
+    
 
 
 }
