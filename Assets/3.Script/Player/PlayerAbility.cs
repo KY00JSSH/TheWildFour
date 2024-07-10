d9fba0bed722bdf07c1c12486ca1fc7152220a34
HongHyeon <toinbee3@naver.com>
2024-07-10 19:23:51 +0900

    Create PlayerAbility.cs

---
+++ Assets/3.Script/Player/PlayerAbility.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerAbility : MonoBehavior {
+    private float playerAttack;
+    private float playerDefense;
+    private float playerGather;
+    private float playerSpeed;
+    private float playerAddInven;
+  
+      
+
+
+}
