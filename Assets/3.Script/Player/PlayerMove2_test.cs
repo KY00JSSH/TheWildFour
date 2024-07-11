d416f36196de5aee2b4ec3b0ee99a1f05c1cacdc
박지훈 <enterpd20@gmail.com>
2024-07-11 13:58:51 +0900

    [Update] Player씬에서 캐릭터 움직임 스크립트 병합 및 테스트 완료

--- Assets/3.Script/Player/PlayerMove2_test.cs
+++ Assets/3.Script/Player/PlayerMove2_test.cs
@@ -2,31 +2,40 @@ using UnityEngine;
 
 public class PlayerMove2_test : MonoBehaviour
 {
-    [SerializeField] private Animator player_ani; //캐릭터 애니메이션을 위해 추가 - 지훈 수정 240708 10:59
     private Rigidbody playerRigid;
-    [SerializeField] private LayerMask layerMask;/**/
     private Vector3 targetPosition;
+    [SerializeField] private Animator player_ani; //캐릭터 애니메이션을 위해 추가 - 지훈 수정 240708 10:59
+    [SerializeField] private LayerMask layerMask;/**/
 
     private float InputX, InputZ;
     private const float constMoveSpeed = 2f;
     [SerializeField] float playerMoveSpeed = 1f, playerDashSpeed = 2.5f;
 
+    public static bool isMove { get; private set; }     // 외부 스크립트에서 현재 이동 상태를 알 수 있는 Flag
+
     private bool isAvailableDash = true;    // 대쉬 게이지에 따라서 설정되는 Flag
 
-    public void SetDash()
-    {
-        isAvailableDash = true;
-    }
-    public void ResetDash()
-    {
-        isAvailableDash = false;
-    }
+    public void SetDash() { isAvailableDash = true; }
+    public void ResetDash() { isAvailableDash = false; }
+
+    // TODO : 대시 게이지 UI 구현. 0707
+    private float TotalDashGage, CurrentDashGage, DecDashGage, IncDashGage;
+    private float defaultDashGage = 10f, defaultDecDashGage = 8f, defaultIncDashGage = 2f;
 
     private void Awake()
-    {
-       
+    {       
         playerRigid = GetComponent<Rigidbody>();
         player_ani = GetComponentInChildren<Animator>(); //캐릭터 애니메이션을 위해 추가 - 지훈 수정 240708 10:59
+    }
+    private void Start()
+    {
+        isMove = false;
+
+        // TODO : JSON 구현 되면 default를 Save된 값으로 바꿀 것
+        TotalDashGage = defaultDashGage;
+        CurrentDashGage = TotalDashGage;
+        DecDashGage = defaultDecDashGage;
+        IncDashGage = defaultIncDashGage;
     }
 
     private void FixedUpdate()
@@ -38,7 +47,18 @@ public class PlayerMove2_test : MonoBehaviour
         //else
         //    Move(playerMoveSpeed);
 
-        Move();        
+        //Move();        
+
+        if (isAvailableDash && Input.GetKey(KeyCode.LeftShift))
+        {
+            Dash(true);
+            Move();
+        }
+        else
+        {
+            Dash(false);
+            Move();
+        }
     }
 
     private void LookatMouse()
@@ -57,11 +77,23 @@ public class PlayerMove2_test : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);/**/            
         }
     }
+        private void Dash(bool isDash)
+    {
+        if (isDash) CurrentDashGage -= DecDashGage * Time.deltaTime;
+        else CurrentDashGage += IncDashGage * Time.deltaTime;
+        CurrentDashGage = Mathf.Clamp(CurrentDashGage, 0, TotalDashGage);
+
+        if (CurrentDashGage == 0) ResetDash();
+        else if (CurrentDashGage == TotalDashGage) SetDash();
+    }
 
     private void Move(/*float speed*/)
     {
         InputX = Input.GetAxisRaw("Horizontal");
         InputZ = Input.GetAxisRaw("Vertical");
+
+        if (InputX != 0 || InputZ != 0) isMove = true;
+        else isMove = false;
 
         //targetPosition = new Vector3(
         //    playerRigid.position.x + InputX * Time.deltaTime * constMoveSpeed * speed,
