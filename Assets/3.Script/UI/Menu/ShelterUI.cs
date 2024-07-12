f45ed046fd3ae5b90a4ffa4a6a6a4dc2170c418e
김수주 <tnwn1122@daum.net>
2024-07-13 02:46:00 +0900

    [Test] skill 버튼클릭 테스트 중

---
+++ Assets/3.Script/UI/Menu/ShelterUI.cs
@@ -0,0 +1,173 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+// 스킬 레벨업 스프라이트 파일 불러오기
+[System.Serializable]
+public class SkillCountImg {
+
+    private Sprite[] skillSprites;
+
+    [SerializeField] public List<Sprite>[] skillCnt = new List<Sprite>[8];
+
+
+    public void Init(string path) {
+        for (int i = 0; i < skillCnt.Length; i++) {
+            skillCnt[i] = new List<Sprite>();
+        }
+        LoadSprites(path);
+        SeparateSprites();
+    }
+    private void LoadSprites(string path) {
+        skillSprites = Resources.LoadAll<Sprite>(path);
+
+
+        if (skillSprites.Length == 0) Debug.LogError("경로 오류 : " + path);
+        else Debug.Log(skillSprites.Length + " 현재 스프라이트 경로: " + path);
+    }
+
+    private void SeparateSprites() {
+        for (int i = 0; i < skillSprites.Length; i++) {
+            string spriteName = skillSprites[i].name;
+            char lastChar = spriteName[spriteName.Length - 1];
+
+            if (char.IsDigit(lastChar)) {
+                int number = lastChar - '0';
+                if (number >= 0 && number < skillCnt.Length) skillCnt[number].Add(skillSprites[i]);
+            }
+            else Debug.Log($"Sprite {spriteName} 마지막 글자가 숫자 아님");
+        }
+    }
+}
+
+public class ShelterUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    private ShelterManager shelterManager;
+
+    [SerializeField] private string spritesPath = "4.Sprite/UI/Shelter_Upgrade/SkillCount";
+    [SerializeField] private SkillCountImg skillCountImg;
+
+    [SerializeField] private GameObject shelterLevelText; // 스킬 위 레벨 표시
+    [SerializeField] private Text shelterLevel;       // shelter 레벨 표시
+    [SerializeField] private GameObject shelterbuttons;
+
+    [SerializeField] private Shelter_Tooltip shelter_Tooltip;
+
+    private void Awake() {
+        shelter_Tooltip = GetComponent<Shelter_Tooltip>();
+        shelterManager = FindObjectOfType<ShelterManager>();
+        shelterLevelText = transform.GetChild(2).gameObject;
+        skillCountImg.Init(spritesPath);
+    }
+
+    private void OnEnable() {
+        ShelterLevel_Alpha();
+        ShelterLevel_AlphaBtns();
+    }
+
+
+    // ShelterManager 레벨받아와서 Text 알파값 조정
+    public void ShelterLevel_Alpha() {
+        for (int i = 0; i < shelterLevelText.transform.childCount; i++) {
+            char shelterLevelTextChar = shelterLevelText.transform.GetChild(i).name[0];
+            int shelterLevelCount = 0;
+            if (char.IsDigit(shelterLevelTextChar)) {
+                shelterLevelCount = shelterLevelTextChar - '0';
+
+                Text text = shelterLevelText.transform.GetChild(i).GetComponent<Text>();
+                Color color = text.color;
+
+                if (shelterLevelCount > shelterManager.ShelterLevel) color.a = 0.5f;
+                else color.a = 1f;
+
+                text.color = color;
+            }
+        }
+    }
+
+
+    // ShelterManager 레벨받아와서 버튼 잠김 Sprite 변경
+    public void ShelterLevel_AlphaBtns() {
+
+        shelterLevel.text = string.Format("<size=50>{0}</size>\n<size=30>레벨</size>", shelterManager.ShelterLevel);
+
+        for (int i = 0; i < shelterbuttons.transform.childCount; i++) {
+            GameObject gameObject = shelterbuttons.transform.GetChild(i).gameObject;
+            for (int j = 3; j < gameObject.transform.childCount; j++) {
+
+                GameObject buttonImg = gameObject.transform.GetChild(j).GetChild(1).gameObject;
+                Color color = buttonImg.GetComponent<Image>().color;
+
+                if ((j - 2) > shelterManager.ShelterLevel) {
+                    color.a = 0.5f;
+                    gameObject.transform.GetChild(j).Find("LockImg").gameObject.SetActive(true);
+                }
+                else {
+                    color.a = 1f;
+                    gameObject.transform.GetChild(j).Find("LockImg").gameObject.SetActive(false);
+                }
+
+                buttonImg.GetComponent<Image>().color = color;
+            }
+        }
+    }
+
+    //TODO: 해당 버튼이 눌리면 스킬 불빛 변경 할 것
+    /*
+     1. 해당 버튼이 눌리면 버튼의 하위객체 3번의 이름 마지막 글자 확인 -> 스킬카운트 갯수
+     2. 해당 글자의 숫자로 SkillCountImg클래스의 스프라이트 list배열 불러오기
+     3. 현재 스프라이트 이름의 마지막과 배열의 개수를 비교하여 마지막 이미지라면 return
+     4. 아니라면 교체
+     
+     */
+    public void HandleButtonClick(GameObject clickedButton) {
+        Debug.Log("HandleButtonClick called");
+
+        Debug.Log("Pointer Click Object: " + clickedButton.name);
+        Button btn = clickedButton.GetComponent<Button>();
+        if (btn != null) {
+            Debug.Log(btn.gameObject.name + " - Mouse Clikck");
+            string nowBtnSpriteName = btn.transform.GetChild(2).name;
+            char lastChar = nowBtnSpriteName[nowBtnSpriteName.Length - 1];
+
+            if (char.IsDigit(lastChar)) {
+                int skillCntNum = lastChar - '0'; //  스프라이트 list배열의 스킬 카운트계산
+
+                List<Sprite> skillSprites = skillCountImg.skillCnt[skillCntNum];
+                int spriteIndex = skillSprites.FindIndex(sprite => sprite.name == nowBtnSpriteName);
+
+                if (spriteIndex > (skillCountImg.skillCnt[skillCntNum].Count - 1)) return;
+                else btn.transform.GetChild(2).GetComponent<Image>().sprite = skillSprites[spriteIndex + 1];
+            }
+        }
+        else {
+            Debug.Log("Not a button");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        Debug.Log("OnPointerEnter called");
+        if (eventData.pointerEnter != null) {
+            Button btn = eventData.pointerEnter.GetComponent<Button>();
+            if (btn != null) {
+                Debug.Log(btn.gameObject.name + " - Mouse enter");
+                shelter_Tooltip.ShelterTooltipShow(btn.gameObject);
+
+                Debug.Log("dictionaryKey 확인" + shelter_Tooltip.dictionaryKey); ;
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        Debug.Log("OnPointerExit called");
+        if (eventData.pointerCurrentRaycast.gameObject != null) {
+            Button btn = eventData.pointerCurrentRaycast.gameObject.GetComponent<Button>();
+            if (btn != null) {
+                Debug.Log(btn.gameObject.name + " - Mouse exit");
+                shelter_Tooltip.dictionaryKey = 0;
+            }
+        }
+    }
+}
--- Assets/3.Script/UI/Menu/Shelter_Tooltip.cs
+++ Assets/3.Script/UI/Menu/Shelter_Tooltip.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 using UnityEngine.EventSystems;
+public class ButtonTest : MonoBehaviour, IPointerClickHandler {
+    public void OnPointerClick(PointerEventData eventData) {
+        Debug.Log("Button Clicked!");
+    }
+}
 
 public class SkillInfo {
     public string Title;
@@ -16,34 +21,26 @@ public class SkillInfo {
 }
 
 
-public class Shelter_Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class Shelter_Tooltip : MonoBehaviour {
     Dictionary<int, SkillInfo> shelterTooltip = new Dictionary<int, SkillInfo>();
-    private int dictionaryKey;
+    public int dictionaryKey;
 
     [SerializeField] private GameObject ShelterTooltip;
     private Image tooltipImg;
     private Text tooltipTitle;
     private Text tooltipMain;
-    private int shelterLevel = 0; // 그 저장 및 불러오기에서 
-    [SerializeField] private GameObject shelterLevelText;
-    private ShelterManager shelterManager;
-
 
     public TextAsset textFile;
     [SerializeField] private List<SkillInfo> skilltexts = new List<SkillInfo>();
 
     private void Awake() {
-        shelterManager = FindObjectOfType<ShelterManager>();
         TextRead();
         tooltipImg = ShelterTooltip.transform.GetChild(0).GetChild(0).GetComponent<Image>();
         tooltipTitle = ShelterTooltip.transform.GetChild(1).GetComponent<Text>();
         tooltipMain = ShelterTooltip.transform.GetChild(0).GetChild(1).GetComponent<Text>();
-        shelterLevelText = transform.GetChild(2).gameObject;
     }
 
-    private void FixedUpdate() {
-        if (shelterManager.isShelterLevelUp || shelterLevel != shelterManager.ShelterLevel) { ShelterLevel_Alpha(); shelterManager.isShelterLevelUp = false; }
-    }
+
 
     // 설명글  dictionary에 저장
     private void TextRead() {
@@ -56,13 +53,14 @@ public class Shelter_Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExit
             skilltexts.Add(newSkillInfo);
             shelterTooltip.Add(i, skilltexts[i]);
         }
+
     }
 
+    /*
     // 하위 버튼 마우스 위치 확인
     public void OnPointerEnter(PointerEventData eventData) {
         Debug.Log("OnPointerEnter called");
         if (eventData.pointerEnter != null) {
-            Debug.Log("Pointer Enter Object: " + eventData.pointerEnter.name);
             Button btn = eventData.pointerEnter.GetComponent<Button>();
             if (btn != null) {
                 Debug.Log(btn.gameObject.name + " - Mouse enter");
@@ -70,32 +68,21 @@ public class Shelter_Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExit
 
                 Debug.Log("dictionaryKey 확인" + dictionaryKey); ;
             }
-            else {
-                Debug.Log("Not a button");
-            }
-        }
-        else {
-            Debug.Log("No object under pointer");
         }
     }
 
     public void OnPointerExit(PointerEventData eventData) {
         Debug.Log("OnPointerExit called");
         if (eventData.pointerCurrentRaycast.gameObject != null) {
-            Debug.Log("Pointer Exit Object: " + eventData.pointerCurrentRaycast.gameObject.name);
             Button btn = eventData.pointerCurrentRaycast.gameObject.GetComponent<Button>();
             if (btn != null) {
                 Debug.Log(btn.gameObject.name + " - Mouse exit");
                 dictionaryKey = 0;
             }
-            else {
-                Debug.Log("Exited non-button object");
-            }
         }
-        else {
-            Debug.Log("Pointer exited to no object");
-        }
+
     }
+    */
 
     // dictionary의 key 값 확인
     // 찾아진 오브젝트의 이름을 받아서 특정 포함되어있으면 값 더해서 리턴 
@@ -104,30 +91,19 @@ public class Shelter_Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExit
         char lastChar = btn.name[btn.name.Length - 1];
         int skillnum = 0;
 
-        if (btn.name.Contains("Move")) {
-            DictionKey = 0;
-        }
-        else if (btn.name.Contains("Attack")) {
-            DictionKey = 5;
-        }
-        else if (btn.name.Contains("Gather")) {
-            DictionKey = 10;
-        }
+        if (btn.name.Contains("Move")) DictionKey = 0;
+        else if (btn.name.Contains("Attack")) DictionKey = 5;
+        else if (btn.name.Contains("Gather")) DictionKey = 10;
 
-        if (char.IsDigit(lastChar)) // Check if the last character is a digit
-        {
-            skillnum = lastChar - '1'; // Convert char to int
-        }
-        else {
-            Debug.Log("Last character is not a number: " + lastChar);
-        }
+        if (char.IsDigit(lastChar)) skillnum = lastChar - '1';
+        else Debug.Log("Last character is not a number: " + lastChar);
 
         DictionKey += skillnum;
         return DictionKey;
     }
 
     // dictionary의 key 값으로 tooltip에 넣기
-    private void ShelterTooltipShow(GameObject btn) {
+    public void ShelterTooltipShow(GameObject btn) {
 
         // 이미지 변경
         tooltipImg.sprite = btn.gameObject.transform.GetChild(1).GetComponent<Image>().sprite;
@@ -137,27 +113,5 @@ public class Shelter_Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExit
         tooltipMain.text = shelterTooltip[dictionaryKey].MainText;
     }
 
-    // 레벨해야하는데 레벨받아와서 Text 알파값 조정
-    private void ShelterLevel_Alpha() {
-        for (int i = 0; i < shelterLevelText.transform.childCount; i++) {
-            char shelterLevelTextChar = shelterLevelText.transform.GetChild(i).name[0];
-            int shelterLevelCount = 0;
-            if (char.IsDigit(shelterLevelTextChar)) {
-                shelterLevelCount = shelterLevelTextChar - '0';
 
-                Text text = shelterLevelText.transform.GetChild(i).GetComponent<Text>();
-                Color color = text.color;
-                if (shelterLevelCount > shelterManager.ShelterLevel) {
-                    color.a = 0.5f;
-                }
-                else {
-                    color.a = 1f;
-                }
-                text.color = color;
-            }
-        }
-
-        shelterLevel = shelterManager.ShelterLevel;
-
-    }
 }
