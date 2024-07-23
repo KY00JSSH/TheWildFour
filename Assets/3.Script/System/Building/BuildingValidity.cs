dcc67c695ab9c44ad66a83d4bc09595ad17a958e
전홍현 <toinbee3@naver.com>
2024-07-23 13:36:07 +0900

    [Fix] BuildingValidity Flag 값 오류 수정

--- Assets/3.Script/System/Building/BuildingValidity.cs
+++ Assets/3.Script/System/Building/BuildingValidity.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BuildingValidity : MonoBehaviour {
     private BuildingCreate buildingCreate;
     private float validAngle = 10f;
-    private bool isTriggered = false;
+    private bool isTriggered;
 
     private void Awake() {
         buildingCreate = GetComponentInParent<BuildingCreate>();
@@ -11,21 +11,41 @@ public class BuildingValidity : MonoBehaviour {
 
     private void Update() {
         if (buildingCreate.isBuild) {
+            /*
+           if ((Mathf.Abs(xRotation) > validAngle && Mathf.Abs(xRotation) < validAngle + 3) ||
+               (Mathf.Abs(zRotation) > validAngle && Mathf.Abs(zRotation) < validAngle + 3) ||
+               (Mathf.Abs(xRotation) > 360 - validAngle && Mathf.Abs(xRotation) < 360 - validAngle + 3) ||
+               (Mathf.Abs(zRotation) > 360 - validAngle && Mathf.Abs(zRotation) < 360 - validAngle + 3)) {
+               Quaternion clampedRotation = Quaternion.Euler(
+                   Mathf.Clamp(xRotation, xRotation, xRotation > 0 ? validAngle : -validAngle),
+                   buildingCreate.Building.transform.rotation.eulerAngles.y,
+                   Mathf.Clamp(zRotation, zRotation, zRotation > 0 ? validAngle : -validAngle)
+                   );
+               Debug.Log(clampedRotation.eulerAngles.x);
+               // 플레이어에 부딪힐 때 각도 값 수정
+
+               buildingCreate.Building.transform.rotation = clampedRotation;
+               buildingCreate.isValidBuild = false;
+           }
+
+           else if (!isTriggered) buildingCreate.isValidBuild = true;
+           */
             float xRotation = buildingCreate.Building.transform.rotation.eulerAngles.x;
             float zRotation = buildingCreate.Building.transform.rotation.eulerAngles.z;
 
-            if ((Mathf.Abs(xRotation) > validAngle && Mathf.Abs(xRotation) < validAngle + 3)||
-                (Mathf.Abs(zRotation) > validAngle && Mathf.Abs(zRotation) < validAngle + 3)) {
+            if (xRotation > 180) xRotation -= 360;
+            if (zRotation > 180) zRotation -= 360;
+            if (Mathf.Abs(xRotation) > validAngle || Mathf.Abs(zRotation) > validAngle) {
                 Quaternion clampedRotation = Quaternion.Euler(
-                    Mathf.Clamp(xRotation, xRotation, xRotation > 0 ? validAngle : -validAngle),
-                    buildingCreate.Building.transform.rotation.eulerAngles.y,
-                    Mathf.Clamp(zRotation, zRotation, zRotation > 0 ? validAngle : -validAngle)
-                    );
+                        Mathf.Clamp(xRotation, -validAngle, validAngle),
+                        buildingCreate.Building.transform.rotation.eulerAngles.y,
+                        Mathf.Clamp(zRotation, -validAngle, validAngle));
 
                 buildingCreate.Building.transform.rotation = clampedRotation;
                 buildingCreate.isValidBuild = false;
             }
-            else if (!isTriggered) buildingCreate.isValidBuild = true;
+            else if (isTriggered) buildingCreate.isValidBuild = false;
+            else buildingCreate.isValidBuild = true;
         }
     }
 
