39bdf267fd4a33f2b9d48f64c8e32c9770e2dbff
박지훈 <enterpd20@gmail.com>
2024-08-06 17:50:00 +0900

    [Fix] 메인 타이틀씬 BGM 재생 수정

--- Assets/3.Script/Scene/Main/GameStart.cs
+++ Assets/3.Script/Scene/Main/GameStart.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class GameStart : MonoBehaviour
 {
     private void Start() {
-        AudioManager.instance.PlayBGM(AudioManager.Bgm.Title_music_LastSummer,3);
+        AudioManager.instance.PlayBGM(AudioManager.Bgm.Title_music_LastSummer, 2);
     }
     public void PlayerSelect() {
         // 로딩 씬을 불러옴 - 로딩씬에서 비동기로 게임씬 부름
